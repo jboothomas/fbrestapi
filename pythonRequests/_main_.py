41e2e02309ce85a10e3e451b951b1c0ac0edd1de
jthomas <jthomas@purestorage.com>
2023-07-11 10:26:42 +0200

    api_version as def

--- pythonRequests/_main_.py
+++ pythonRequests/_main_.py
@@ -1,5 +1,6 @@
 from login import login
 from logout import logout
+from api_version import api_version
 
 FB_IP = '<YOUR_FLASHBLADE_IP>'
 API_TOKEN = '<YOUR_FB_API_TOKEN>'
@@ -10,9 +11,20 @@ token = login(FB_IP, API_TOKEN)
 
 if token is not None:
     print(f'Login succesfull received x-auth-token: {token}')
+
+    # Get latest api_version
+    versions = api_version(FB_IP)
+    if versions is not None:
+        useVersion = versions[-1]
+        print(f'RestAPI version will be: {useVersion}')
+    else:
+        print('Failed to get api versions')
+
     # Use the token to logout
     logout_status = logout(FB_IP, token)
     print(logout_status)
+
+
 
 else:
     print("Failed to authenticate")
