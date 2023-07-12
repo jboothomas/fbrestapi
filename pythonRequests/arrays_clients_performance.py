4bde6721399b0a247b828978f4209ef81850183d
jthomas <jthomas@purestorage.com>
2023-07-12 17:08:27 +0200

    arrayclientsperfromance

--- pythonRequests/arrays_clients_performance.py
+++ pythonRequests/arrays_clients_performance.py
@@ -2,31 +2,30 @@ import requests
 import json
 
 
-def arrays_clients_performance(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PAYLOAD):
+def arrays_clients_performance(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PARAMS, PAYLOAD, VALIDATE_SSL):
 
-    url = f"https://{FB_IP}/api/{API_VERSION}/arrays/clients/performance"
-    
-    if METHOD not in ['GET']:
-        print(f'The method "{METHOD}" is not valid for {url}.')
-        return
-    
-    headers = {
-      'x-auth-token': X_AUTH_TOKEN
+    ENDPOINT = f'api/{API_VERSION}/arrays/clients/performance'
+    VALIDATE_METHODS = ['GET']
+    HEADERS = {
+        'x-auth-token': X_AUTH_TOKEN
     }
-    # Convert payload to JSON
-    payload = json.dumps(PAYLOAD)
 
-    response = requests.request(
-        METHOD,
-        url, 
-        headers=headers, 
-        data=payload,
-        verify=False  # consider removing this if your FB has a valid SSL cert
-    )
+    result = send_request(FB_IP, ENDPOINT, METHOD, HEADERS, PARAMS, PAYLOAD, arrays_clients_performance_validateparams, VALIDATE_METHODS, VALIDATE_SSL)
+    return result
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f'{METHOD} request to {url} failed with status code {response.status_code}')
-        return None
+def arrays_clients_performance_validateparams(METHOD, PARAMS):
+
+    # Define the set of all possible fields based on method
+    if METHOD in ['GET']:
+
+        valid_fields = {'filter', 'limit', 'names', 'sort', 'total_only'}
+    
+    
+    # Check if any field in params is not in possible_fields
+    for field in PARAMS:
+        if field not in valid_fields:
+            print(f"Error: Unknown field '{field}'.")
+            return False
+
+    # If no errors were found, the params are valid
+    return True
