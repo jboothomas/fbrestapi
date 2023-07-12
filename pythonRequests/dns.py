c34d5755788a2ae07e62f6d15b42ba7313352c58
jthomas <jthomas@purestorage.com>
2023-07-12 09:08:45 +0200

    dns

---
+++ pythonRequests/dns.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+def dns(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PAYLOAD):
+
+    ## Example application/json payload
+    #{
+    #  "domain": "example.com",
+    #  "nameservers": [
+    #    "192.168.0.125"
+    #  ]
+    #}
+
+    url = f"https://{FB_IP}/api/{API_VERSION}/dns"
+    
+    if METHOD not in ['GET', 'PATCH']:
+        print(f'The method "{METHOD}" is not valid for {url}.')
+        return
+    
+    headers = {
+      'x-auth-token': X_AUTH_TOKEN
+    }
+    # Convert payload to JSON
+    payload = json.dumps(PAYLOAD)
+
+    response = requests.request(
+        METHOD,
+        url, 
+        headers=headers, 
+        data=payload,
+        verify=False  # consider removing this if your FB has a valid SSL cert
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f'{METHOD} request to {url} failed with status code {response.status_code}')
+        return None
