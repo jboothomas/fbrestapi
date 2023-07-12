386dc97ad67de89c12bb5149ad8e99088c618b9b
jthomas <jthomas@purestorage.com>
2023-07-12 17:23:06 +0200

    moved client perf under arrays

--- pythonRequests/arrays.py
+++ pythonRequests/arrays.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from _sendrequest_ import send_request
 
 
 def arrays(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PAYLOAD):
@@ -321,3 +322,31 @@ def arrays_supportedtimezones(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PAYLOAD)
         errormessage = data['errors'][0]['message']
         print(f'{METHOD} request to {url} failed with status code {response.status_code} error message: {errormessage}')
         return None
+
+def arrays_clients_performance(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PARAMS, PAYLOAD, VALIDATE_SSL):
+
+    ENDPOINT = f'api/{API_VERSION}/arrays/clients/performance'
+    VALIDATE_METHODS = ['GET']
+    HEADERS = {
+        'x-auth-token': X_AUTH_TOKEN
+    }
+
+    result = send_request(FB_IP, ENDPOINT, METHOD, HEADERS, PARAMS, PAYLOAD, arrays_clients_performance_validateparams, VALIDATE_METHODS, VALIDATE_SSL)
+    return result
+
+def arrays_clients_performance_validateparams(METHOD, PARAMS):
+
+    # Define the set of all possible fields based on method
+    if METHOD in ['GET']:
+
+        valid_fields = {'filter', 'limit', 'names', 'sort', 'total_only'}
+    
+    
+    # Check if any field in params is not in possible_fields
+    for field in PARAMS:
+        if field not in valid_fields:
+            print(f"Error: Unknown field '{field}'.")
+            return False
+
+    # If no errors were found, the params are valid
+    return True
