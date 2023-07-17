ae956266c739d27dee26dcef9691a5e254842e4f
jthomas <jthomas@purestorage.com>
2023-07-17 10:20:36 +0200

    removed query params validation - performed in restAPI

--- pythonRequests/hardwareconnectors.py
+++ pythonRequests/hardwareconnectors.py
@@ -15,32 +15,9 @@ def hardwareconnectors(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PARAMS, PAYLOAD
         'x-auth-token': X_AUTH_TOKEN
     }
 
-    result = send_request(FB_IP, ENDPOINT, METHOD, HEADERS, PARAMS, PAYLOAD, hardwareconnectors_validateparams, VALIDATE_METHODS, VALIDATE_SSL)
+    result = send_request(FB_IP, ENDPOINT, METHOD, HEADERS, PARAMS, PAYLOAD, VALIDATE_METHODS, VALIDATE_SSL)
     return result
 
-def hardwareconnectors_validateparams(METHOD, PARAMS):
-
-    # Define the set of all possible fields based on method
-    if METHOD in ['GET']:
-        if 'ids' in PARAMS and 'names' in PARAMS:
-            print("Error: 'ids' and 'names' cannot be provided at the same time.")
-            return False
-        valid_fields = {'continuation_token', 'filter', 'ids', 'limit', 'names', 'offset', 'sort'}
-
-    if METHOD in ['PATCH']:
-        if 'ids' in PARAMS and 'names' in PARAMS:
-            print("Error: 'ids' and 'names' cannot be provided at the same time.")
-            return False
-        valid_fields = {'ids', 'names'}
-        
-    # Check if any field in params is not in possible_fields
-    for field in PARAMS:
-        if field not in valid_fields:
-            print(f"Error: Unknown field '{field}'.")
-            return False
-
-    # If no errors were found, the params are valid
-    return True 
 
 def hardwareconnectors_performance(METHOD, FB_IP, X_AUTH_TOKEN, API_VERSION, PARAMS, PAYLOAD, VALIDATE_SSL):
 
